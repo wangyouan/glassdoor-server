acbc9f820c0e1635e14d25f8c3f391625361898b
wangyouan <wangyouan0629@hotmail.com>
2025-06-21 14:17:00 +0800

    update path structure

---
+++ Constants/path_info.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+# @Filename: path_info
+# @Date: 2025/6/21
+# @Author: Mark Wang
+# @Email: wangyouan@gamil.com
---
+++ Reference/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+# @Filename: __init__.py
+# @Date: 2025/6/21
+# @Author: Mark Wang
+# @Email: wangyouan@gamil.com
