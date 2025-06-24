be7d7ed5f057b86d958f9e0f281fd81bf0607b40
wangyouan <wangyouan0629@hotmail.com>
2025-06-24 20:18:14 +0800

    fill in missing information

--- PythonCode/step02_fillin_missing_conm_information.py
+++ PythonCode/step02_fillin_missing_conm_information.py
@@ -18,7 +18,7 @@ import numpy as np
 from Constants import Constants as const
 
 if __name__ == '__main__':
-    ue_with_web = pd.read_excel(os.path.join(const.TEMP_PATH, '20250622_ue_with_glassdoor_web.xlsx'))
+    ue_with_web = pd.read_excel(os.path.join(const.TEMP_PATH, '20250624_ue_with_glassdoor_web.xlsx'))
     ctat_df = pd.read_csv(os.path.join(const.COMPUSTAT_PATH, '1950_2024_ctat_firm_identifiers.zip'))
     # missing_index = ue_with_web[ue_with_web[const.GVKEY].isnull()].index
     #
@@ -28,6 +28,7 @@ if __name__ == '__main__':
     #     ue_with_web.loc[i, 'conm'] = tmp_ctat_name
 
     company_name_map = ctat_df[['gvkey', 'conml']].dropna(how='any').drop_duplicates().set_index('gvkey')['conml']
+    company_name_map2 = ctat_df[['gvkey', 'conm']].dropna(how='any').drop_duplicates().set_index('gvkey')['conm']
     url_gvkey_map = ctat_df[['gvkey', 'weburl']].dropna(how='any').drop_duplicates().set_index('gvkey')['weburl']
 
     # 4. 填充缺失的公司名称
@@ -35,6 +36,9 @@ if __name__ == '__main__':
     ue_with_web['conml'] = np.nan
     ue_with_web['conml'] = ue_with_web['conml'].fillna(
         ue_with_web[const.GVKEY].map(company_name_map)
+    )
+    ue_with_web['conm'] = ue_with_web['conm'].fillna(
+        ue_with_web[const.GVKEY].map(company_name_map2)
     )
     ue_with_web['weburl'] = np.nan
     ue_with_web['weburl'] = ue_with_web['weburl'].fillna(
